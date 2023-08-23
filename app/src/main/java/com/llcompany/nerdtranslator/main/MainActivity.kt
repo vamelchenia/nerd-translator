f3448f7f6e4504dac20914ed65f0be822c4b87a6
Steyrix <steyrixio@gmail.com>
2023-08-23 15:32:37 +0400

    Inherit MainActivity from ComponentActivity. Create stub for first screen. Edit Manifest

--- app/src/main/java/com/llcompany/nerdtranslator/main/MainActivity.kt
+++ app/src/main/java/com/llcompany/nerdtranslator/main/MainActivity.kt
@@ -3,30 +3,25 @@ package com.llcompany.nerdtranslator.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.rijksdataexplorer.R
-import com.llcompany.nerdtranslator.core.NerdTranslatorApp
 import com.llcompany.nerdtranslator.main.ui.theme.NerdTranslatorTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         setContent {
-            MaterialTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-
+            NerdTranslatorTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Nerd Translator")
                 }
             }
         }
