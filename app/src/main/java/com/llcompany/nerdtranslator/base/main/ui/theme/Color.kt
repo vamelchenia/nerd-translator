7352d86e08035c6cee223b48b4c70ee207c6a53f
Steyrix <steyrixio@gmail.com>
2023-09-03 16:08:57 +0400

    Move theme, colors and types to another package. Define colors according to design

---
+++ app/src/main/java/com/llcompany/nerdtranslator/base/main/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.llcompany.nerdtranslator.base.main.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val colorPrimaryDark = Color(0xFF001F35)
+val colorSecondaryBlue = Color(0xFF5A798F)
+val colorTertiaryBlue = Color(0xFF99AEBD)
+val colorNeutral = Color(0xFFA3ABD5)
+val colorExtraLightBlue = Color(0xFFDBE8F0)
+val colorBgExtraLightBlue = Color(0xFFEEF4F8)
+val colorWhite = Color(0xFFFFFFFF)
+val colorBlue = Color(0xFF3B7091)
+val colorRed = Color(0xFFF2575D)
+
+val buttonColorPrimaryNormal = Color(0xFFF2575D)
+val buttonColorPrimaryPressed = Color(0xFFCC4341)
+val buttonColorSecondaryNormal = Color(0xFF99AEBD)
+val buttonColorSecondaryPressed = Color(0xFF5A798F)
+
+val textColorPrimary = Color(0xFF001F35)
+val textColorSecondary = Color(0xFF5A798F)
+val textColorTertiary = Color(0xFF99AEBD)
+val textColorNeutral = Color(0xFFA3ABD5)
+val textColorOnButton = Color(0xFFFFFFFF)
+val textColorError = Color(0xFFF2575D)
