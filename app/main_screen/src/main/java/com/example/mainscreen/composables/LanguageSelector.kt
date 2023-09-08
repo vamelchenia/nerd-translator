67a02bf34b3ab58198cb86b2aa00332fc3c4da27
Steyrix <steyrixio@gmail.com>
2023-09-08 17:57:31 +0400

    Fix shape, icon and paddings for LanguageSelector

--- app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelector.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelector.kt
@@ -1,16 +1,24 @@
 package com.example.mainscreen.composables
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.mainscreen.R
+
+private val roundingSize = 5.dp
+private val buttonPaddings = PaddingValues(30.dp, 10.dp, 20.dp, 10.dp)
 
 @Composable
 fun LanguageSelector(
@@ -18,23 +26,25 @@ fun LanguageSelector(
     isError: Boolean
 ) {
     Box(
-        modifier = Modifier.padding(Dp(8f))
+        modifier = Modifier.padding(8.dp)
     ) {
         Button(
             onClick = {},
-            shape = RoundedCornerShape(5),
+            shape = RoundedCornerShape(roundingSize),
             colors = ButtonDefaults.buttonColors(
                 containerColor = MaterialTheme.colorScheme.tertiary
-            )
+            ),
+            contentPadding = buttonPaddings
         ) {
             Text(
                 text = languageName,
                 color = MaterialTheme.colorScheme.primary
             )
-//            Icon(
-//                imageVector = ,
-//                contentDescription =
-//            )
+            Icon(
+                painter = painterResource(R.drawable.arrow_down_ic_24),
+                contentDescription = stringResource(R.string.language_selector_expand_more),
+                tint = MaterialTheme.colorScheme.secondary
+            )
         }
     }
 }
