aa75f7d98e690896df87d85f84c79ded0c18b441
Steyrix <steyrixio@gmail.com>
2023-09-08 17:11:19 +0400

    Setup colors for LanguageSelector

--- app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelector.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelector.kt
@@ -1,30 +1,41 @@
 package com.example.mainscreen.composables
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 
 @Composable
 fun LanguageSelector(
     languageName: String,
     isError: Boolean
 ) {
-    Button(
-        onClick = {},
-        shape = RoundedCornerShape(5),
-        modifier = Modifier.background(
-            color = MaterialTheme.colorScheme.background
-        )
+    Box(
+        modifier = Modifier.padding(Dp(8f))
     ) {
-        Text(
-            color = MaterialTheme.colorScheme.primary,
-            text = languageName
-        )
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(5),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.tertiary
+            )
+        ) {
+            Text(
+                text = languageName,
+                color = MaterialTheme.colorScheme.primary
+            )
+//            Icon(
+//                imageVector = ,
+//                contentDescription =
+//            )
+        }
     }
 }
 
