7ae5f08e9250b78437f1c795fb4627f6279b0d56
Steyrix <steyrixio@gmail.com>
2023-09-08 16:26:26 +0400

    Start implementing LanguageSelector

---
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelector.kt
@@ -0,0 +1,28 @@
+package com.example.mainscreen.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LanguageSelector(
+    languageName: String,
+    isError: Boolean
+) {
+    Button(
+        onClick = {},
+        shape = RoundedCornerShape(5),
+        modifier = Modifier.background(
+            color = MaterialTheme.colorScheme.background
+        )
+    ) {
+        Text(
+            color = MaterialTheme.colorScheme.primary,
+            text = languageName
+            )
+    }
+}
