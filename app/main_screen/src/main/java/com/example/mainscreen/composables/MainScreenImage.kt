a8a0615369d2aa25cb6bc229be126f92adcdffe4
Steyrix <steyrixio@gmail.com>
2023-09-21 20:06:15 +0400

    Add texts

--- app/main_screen/src/main/java/com/example/mainscreen/composables/MainScreenImage.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/MainScreenImage.kt
@@ -1,14 +1,38 @@
 package com.example.mainscreen.composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.example.mainscreen.R
 
+// TODO: double line
+// TODO: text line interval
 @Composable
 fun MainScreenImage() {
-    Image(
-        painter = painterResource(R.drawable.main_screen_image),
-        contentDescription = stringResource(R.string.main_screen_image))
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.main_screen_image),
+            contentDescription = stringResource(R.string.main_screen_image)
+        )
+
+        Text(
+            text = stringResource(R.string.under_image_hint_primary),
+            color = MaterialTheme.colorScheme.secondary,
+            fontSize = 14.sp
+        )
+
+        Text(
+            text = stringResource(R.string.under_image_hint_secondary),
+            color = MaterialTheme.colorScheme.tertiary,
+            fontSize = 12.sp
+        )
+    }
 }
