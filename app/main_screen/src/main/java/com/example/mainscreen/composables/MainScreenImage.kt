87688852b192351d2ddb024c526092f4e7f82bfa
Steyrix <steyrixio@gmail.com>
2023-09-24 15:39:30 +0400

    Add margins to text views

--- app/main_screen/src/main/java/com/example/mainscreen/composables/MainScreenImage.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/MainScreenImage.kt
@@ -2,19 +2,25 @@ package com.example.mainscreen.composables
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mainscreen.R
 
-// TODO: double line
-// TODO: text line interval
 @Composable
 fun MainScreenImage() {
+    val topHintMargins = PaddingValues(0.dp, 8.dp, 0.dp, 8.dp)
+    val bottomHintMargins = PaddingValues(0.dp, 0.dp, 0.dp, 8.dp)
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -26,13 +32,19 @@ fun MainScreenImage() {
         Text(
             text = stringResource(R.string.under_image_hint_primary),
             color = MaterialTheme.colorScheme.secondary,
-            fontSize = 14.sp
+            fontSize = 14.sp,
+            lineHeight = 16.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(topHintMargins)
         )
 
         Text(
             text = stringResource(R.string.under_image_hint_secondary),
             color = MaterialTheme.colorScheme.tertiary,
-            fontSize = 12.sp
+            fontSize = 12.sp,
+            lineHeight = 14.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottomHintMargins)
         )
     }
 }
