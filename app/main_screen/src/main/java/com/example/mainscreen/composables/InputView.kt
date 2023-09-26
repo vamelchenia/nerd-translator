9bcc635a0e34d7b86cbae736063d896acf6f120b
Steyrix <steyrixio@gmail.com>
2023-09-26 16:20:28 +0400

    Use TextField from material3 and use DefaultTextFieldColors composable for its recoloring

--- app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
@@ -1,17 +1,15 @@
 package com.example.mainscreen.composables
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.TextField
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
 private val roundingSize = 20.dp
@@ -19,19 +17,19 @@ private val roundingSize = 20.dp
 @Composable
 fun DefaultTextFieldColors() =
     TextFieldDefaults.colors(
-        focusedContainerColor = MaterialTheme.colorScheme.background
+        focusedContainerColor = MaterialTheme.colorScheme.background,
+        unfocusedContainerColor = MaterialTheme.colorScheme.background
     )
 
 @Composable
-fun InputView() {
+fun InputView(
+    textFieldColors: TextFieldColors = DefaultTextFieldColors()
+) {
     var textValue by remember { mutableStateOf("Sample text") }
 
     TextField(
-        modifier = Modifier.background(
-            color = MaterialTheme.colorScheme.background,
-            shape = RoundedCornerShape(roundingSize)
-        ),
-        value = textValue,
-        onValueChange = { textValue = it }
+        value = TextFieldValue(textValue),
+        onValueChange = { textValue = it.text },
+        colors = textFieldColors
     )
 }
