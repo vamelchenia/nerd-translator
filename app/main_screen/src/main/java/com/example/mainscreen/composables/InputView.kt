7409e3f433f30a28467c61ae02c6760b26d2ca59
Steyrix <steyrixio@gmail.com>
2023-09-27 18:42:45 +0400

    Fill max width with input view and round corners

--- app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
@@ -1,5 +1,7 @@
 package com.example.mainscreen.composables
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
@@ -9,7 +11,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+
+private val roundingSize = 20.dp
 
 @Composable
 fun defaultTextFieldColors() =
@@ -27,6 +33,8 @@ fun InputView(
     TextField(
         value = textFieldValue,
         onValueChange = { textFieldValue = it },
-        colors = textFieldColors
+        colors = textFieldColors,
+        modifier = Modifier.fillMaxSize(),
+        shape = RoundedCornerShape(roundingSize, roundingSize, 0.dp, 0.dp)
     )
 }
