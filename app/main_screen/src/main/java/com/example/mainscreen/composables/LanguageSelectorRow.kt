5d550a7af2924ce7ae4ed40bd5caff0cc0f58c99
Steyrix <steyrixio@gmail.com>
2023-09-27 18:49:46 +0400

    Add bottom margins for selectors

--- app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelectorRow.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelectorRow.kt
@@ -1,25 +1,31 @@
 package com.example.mainscreen.composables
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.mainscreen.MainScreenContract
 import com.example.mainscreen.R
 
 @Composable
 fun LanguageSelectorRow(
     sourceState: MainScreenContract.LanguageSelectorState,
-    destinationState: MainScreenContract.LanguageSelectorState
+    destinationState: MainScreenContract.LanguageSelectorState,
+    rowModifier: Modifier
 ) {
     val iconPainter = painterResource(R.drawable.change_icon)
     val colorBlue = Color(0xFF3B7091)
 
     Row(
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = rowModifier
     ) {
         LanguageSelector(state = sourceState)
 
--- app/main_screen/src/main/java/com/example/mainscreen/composables/MainScreen.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/MainScreen.kt
@@ -1,8 +1,12 @@
 package com.example.mainscreen.composables
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.mainscreen.MainScreenContract
 import kotlinx.coroutines.flow.Flow
 
@@ -17,9 +21,12 @@ fun MainScreen(
     ) {
         MainScreenImage()
 
+        val selectorsPaddingValues = PaddingValues(0.dp, 0.dp, 0.dp, 8.dp)
+
         LanguageSelectorRow(
             sourceState = state.sourceSelectorState,
-            destinationState = state.destinationSelectorState
+            destinationState = state.destinationSelectorState,
+            rowModifier = Modifier.padding(selectorsPaddingValues)
         )
 
         InputView()
