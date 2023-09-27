d5112471fcf4f1f964e702df474fa96d1dbabbae
Steyrix <steyrixio@gmail.com>
2023-09-27 19:03:40 +0400

    Implement placeholder. Implement composable to encapsulate text styling

--- app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
@@ -3,6 +3,7 @@ package com.example.mainscreen.composables
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
@@ -12,8 +13,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mainscreen.R
 
 private val roundingSize = 20.dp
 
@@ -25,16 +31,32 @@ fun defaultTextFieldColors() =
     )
 
 @Composable
+fun inputViewTextStyle(
+    textColor: Color = Color(0xFF001F35)
+) = TextStyle(
+    fontSize = 20.sp,
+    lineHeight = 28.sp,
+    color = textColor
+)
+
+@Composable
 fun InputView(
     textFieldColors: TextFieldColors = defaultTextFieldColors()
 ) {
-    var textFieldValue by remember { mutableStateOf(TextFieldValue("Sample text")) }
+    var textFieldValue by remember { mutableStateOf(TextFieldValue("")) }
 
     TextField(
         value = textFieldValue,
         onValueChange = { textFieldValue = it },
         colors = textFieldColors,
         modifier = Modifier.fillMaxSize(),
-        shape = RoundedCornerShape(roundingSize, roundingSize, 0.dp, 0.dp)
+        shape = RoundedCornerShape(roundingSize, roundingSize, 0.dp, 0.dp),
+        textStyle = inputViewTextStyle(),
+        placeholder = {
+            Text(
+                text = stringResource(R.string.input_view_hint),
+                style = inputViewTextStyle(Color(0xFFA3ADB5))
+            )
+        }
     )
 }
