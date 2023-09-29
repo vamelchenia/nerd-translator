50f7986f11dc7abb94b89403fbd6dd36ce9e9ac0
Steyrix <steyrixio@gmail.com>
2023-09-29 16:29:28 +0400

    Add keyboard button to input view

--- app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/InputView.kt
@@ -2,6 +2,7 @@ package com.example.mainscreen.composables
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,11 +15,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mainscreen.R
 
 private val roundingSize = 20.dp
@@ -46,18 +50,38 @@ fun InputView(
 ) {
     var textFieldValue by remember { mutableStateOf(TextFieldValue("")) }
 
-    TextField(
-        value = textFieldValue,
-        onValueChange = { textFieldValue = it },
-        colors = textFieldColors,
-        modifier = Modifier.fillMaxSize(),
-        shape = RoundedCornerShape(roundingSize, roundingSize, 0.dp, 0.dp),
-        textStyle = inputViewTextStyle(),
-        placeholder = {
-            Text(
-                text = stringResource(R.string.input_view_hint),
-                style = inputViewTextStyle(Color(0xFFA3ADB5))
-            )
-        }
-    )
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val (inputArea, keyboardButton) = createRefs()
+
+        TextField(
+            value = textFieldValue,
+            onValueChange = { textFieldValue = it },
+            colors = textFieldColors,
+            modifier = Modifier
+                .fillMaxSize()
+                .constrainAs(inputArea) {
+                    centerHorizontallyTo(parent)
+                },
+            shape = RoundedCornerShape(roundingSize, roundingSize, 0.dp, 0.dp),
+            textStyle = inputViewTextStyle(),
+            placeholder = {
+                Text(
+                    text = stringResource(R.string.input_view_hint),
+                    style = inputViewTextStyle(Color(0xFFA3ADB5))
+                )
+            }
+        )
+
+        Icon(
+            painter = painterResource(R.drawable.btn_keyboard),
+            contentDescription = stringResource(R.string.language_selector_expand_more),
+            tint = Color.Unspecified,
+            modifier = Modifier.constrainAs(keyboardButton) {
+                centerHorizontallyTo(inputArea)
+                bottom.linkTo(inputArea.bottom, margin = 24.5.dp)
+            }
+        )
+    }
 }
