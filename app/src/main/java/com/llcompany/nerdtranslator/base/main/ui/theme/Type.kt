7b676f9849f8dbbec8e7dc0b8fe31e7076ecbb56
Steyrix <steyrixio@gmail.com>
2023-10-02 20:01:33 +0400

    Start overriding text styles

--- app/src/main/java/com/llcompany/nerdtranslator/base/main/ui/theme/Type.kt
+++ app/src/main/java/com/llcompany/nerdtranslator/base/main/ui/theme/Type.kt
@@ -7,12 +7,28 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+private val interFont = Font(com.example.mainscreen.R.font.inter_black)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily(Font(com.example.mainscreen.R.font.inter_black)),
+        fontFamily = FontFamily(interFont),
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(interFont),
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(interFont),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.5.sp
     )
