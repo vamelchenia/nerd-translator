5ac018969be8ab4d19c2851eddaaf79a6a4ec2d7
Steyrix <steyrixio@gmail.com>
2023-10-04 14:21:12 +0400

    Define more text styles

--- app/src/main/java/com/llcompany/nerdtranslator/base/main/ui/theme/Type.kt
+++ app/src/main/java/com/llcompany/nerdtranslator/base/main/ui/theme/Type.kt
@@ -24,7 +24,7 @@ private val interFontFamily = FontFamily(
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    // bodyLarge style is for short source text
+    // bodyLarge is for short source text
     bodyLarge = TextStyle(
         fontFamily = interFontFamily,
         fontWeight = FontWeight.Normal,
@@ -40,12 +40,85 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = defaultLetterSpacing
     ),
-    // headlineLarge style is for titles of screens (Create a tag, Select tags to duplicate, etc.)
-    headlineLarge = TextStyle(
+    // bodySmall is for language selector text
+    bodySmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // displayLarge is for short destination text
+    displayLarge = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // displayMedium is for long destination text
+    displayMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // displaySmall is for "create a tag and use it..." hint under the input view
+    displaySmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 28.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // titleLarge is for titles of screens (Create a tag, Select tags to duplicate, etc.)
+    titleLarge = TextStyle(
         fontFamily = interFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = defaultLetterSpacing
-    )
+    ),
+    // titleMedium is for tags (e.g. on "select tags" screen)
+    titleMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // titleSmall is for filled buttons (e.g. "create", "done", "paste")
+    titleSmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // labelLarge is for hint labels (e.g. under pre-translate image)
+    labelLarge = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // labelMedium is for smaller hint labels (e.g. under pre-translate image)
+    labelMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // headlineMedium is for dialogs texts
+    headlineMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
+    // headlineSmall is for extra small hints (e.g. like "Currently you add to favorites as tag")
+    headlineSmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 11.sp,
+        letterSpacing = defaultLetterSpacing
+    ),
 )
