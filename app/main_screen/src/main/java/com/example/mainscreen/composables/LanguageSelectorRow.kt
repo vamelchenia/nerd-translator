f15ded73763056d365cbae2759904497c2e901fb
Steyrix <steyrixio@gmail.com>
2023-10-10 13:30:07 +0400

    Delete unused imports

--- app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelectorRow.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelectorRow.kt
@@ -1,8 +1,6 @@
 package com.example.mainscreen.composables
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,7 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.example.mainscreen.MainScreenContract
 import com.example.mainscreen.R
 
