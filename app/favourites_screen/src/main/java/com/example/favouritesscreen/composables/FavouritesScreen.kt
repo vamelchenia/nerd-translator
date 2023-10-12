13d891758c837a2c6ca0a36187022caa6a6a87f9
Valeriia Amelchenia <valeriia.amelchenia@gmail.com>
2023-10-13 02:15:15 +0300

    Fix padding of Favourite Screen

--- app/favourites_screen/src/main/java/com/example/favouritesscreen/composables/FavouritesScreen.kt
+++ app/favourites_screen/src/main/java/com/example/favouritesscreen/composables/FavouritesScreen.kt
@@ -1,13 +1,16 @@
 package com.example.favouritesscreen.composables
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FavouritesScreen() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
     ) {
         FavouritesScreenImage()
         CreateButton()
