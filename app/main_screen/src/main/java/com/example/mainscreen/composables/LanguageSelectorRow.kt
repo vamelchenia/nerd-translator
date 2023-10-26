ee0b42a30d4f87db229bbf2f1b96661adc01fad4
Steyrix <steyrixio@gmail.com>
2023-10-26 19:54:01 +0400

    Wrap swap icon in IconButton

--- app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelectorRow.kt
+++ app/main_screen/src/main/java/com/example/mainscreen/composables/LanguageSelectorRow.kt
@@ -2,6 +2,7 @@ package com.example.mainscreen.composables
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,11 +27,13 @@ fun LanguageSelectorRow(
         LanguageSelector(state = sourceState)
 
         // TODO: make icon clickable and switch states on click
-        Icon(
-            painter = iconPainter,
-            contentDescription = stringResource(R.string.language_selector_row_switch),
-            tint = MaterialTheme.colorScheme.onSurface
-        )
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                painter = iconPainter,
+                contentDescription = stringResource(R.string.language_selector_row_switch),
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        }
 
         LanguageSelector(state = destinationState)
     }
