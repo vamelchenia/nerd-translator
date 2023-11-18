f2c15fa0b21372ef6a9d0f0e7b9005964b8984f7
Steyrix <steyrixio@gmail.com>
2023-11-18 13:41:19 +0400

    Require service as a parameter for repo

--- app/core/src/main/java/com/example/core/network/NerdTranslatorRepositoryImpl.kt
+++ app/core/src/main/java/com/example/core/network/NerdTranslatorRepositoryImpl.kt
@@ -1,4 +1,6 @@
 package com.example.core.network
 
-class NerdTranslatorRepositoryImpl : NerdTranslatorRepository {
+class NerdTranslatorRepositoryImpl(
+    private val service: NerdTranslatorService
+) : NerdTranslatorRepository {
 }
